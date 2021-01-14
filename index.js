9f86d2ccd808cc5a0aafb4d4bc7975bbab1462a7
Varga Adam <vargaad95@gmail.com>
2021-01-14 16:36:20 +0100

    create plugins

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const typescriptBackend = require("./lib/typescript/backend");
 const javascriptBackend = require("./lib/javascript/backend");
+const testing = require("./lib/testing");
 
 module.exports = {
   configs: {
+    testing,
     "typescript-backend": typescriptBackend,
     "javascript-backend": javascriptBackend
   }
